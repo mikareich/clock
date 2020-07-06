54baec0754a6b887cc567aaaef21b32763069a76
Mika Reich <mikareich0@gmail.com>
2020-07-06 22:10:23 +0200

    add clock-face

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ function setHands() {
   const Total_Milliseconds_Of_Day = 86400000
   const Milliseconds_Of_Day =
     new Date().getTime() - new Date().setHours(0, 0, 0, 0)
-  const PassedTime = Milliseconds_Of_Day / Total_Milliseconds_Of_Day
+  const DayProgress = Milliseconds_Of_Day / Total_Milliseconds_Of_Day
 
   // calculate progress in degreses multiplicated by the implementation
-  const HourRotation = PassedTime * 360 * 2
-  const MinuteRotation = PassedTime * 360 * 24
+  const HourRotation = DayProgress * 360 * 2
+  const MinuteRotation = DayProgress * 360 * 24
 
   // subtract css offset (90deg)
   clockHandHour.setAttribute('style', `--rotation:${HourRotation - 90}deg`)
